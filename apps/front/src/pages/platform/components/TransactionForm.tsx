2b96a31b648789fa449e7a6ebe0e72d95b03d9ab
Enmanuel Jarquín <enmisac@gmail.com>
2023-09-15 21:54:24 -0600

    refactor transaction form

--- apps/front/src/pages/platform/components/TransactionForm.tsx
+++ apps/front/src/pages/platform/components/TransactionForm.tsx
@@ -5,7 +5,7 @@ import {
   useDeleteTransaction,
   useMutateTransaction,
 } from "../../../queries/useMutateTransaction";
-import { Button } from "ui";
+import { Button, Field } from "ui";
 
 const transactionTypes = [
   { name: "Expense", value: "EXPENSE" },
@@ -103,7 +103,7 @@ const TransactionForm = ({
     <form className="flex justify-center" onSubmit={handleSubmit}>
       <div className="flex w-[300px] flex-col gap-5">
         <div>
-          <label className="">
+          <label>
             <span className="text-sm text-bold">Type</span>
           </label>
           <Select
@@ -114,7 +114,7 @@ const TransactionForm = ({
           />
         </div>
         <div>
-          <label className="">
+          <label>
             <span className="text-sm text-bold">Category</span>
           </label>
           <Select
@@ -127,29 +127,22 @@ const TransactionForm = ({
             }))}
           />
         </div>
-        <label className="relative block">
-          <span className="text-sm text-bold">Amount</span>
-          <input
-            className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2  pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            placeholder="0"
-            type="number"
-            name="amount"
-            value={values.amount}
-            onChange={handleChange}
-          />
-        </label>
-
-        <label className="relative block">
-          <span className="text-sm text-bold">Note</span>
-          <input
-            className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2  pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            placeholder="Additional notes"
-            type="text"
-            name="note"
-            value={values.note}
-            onChange={handleChange}
-          />
-        </label>
+        <Field
+          label="Amount"
+          placeholder="0"
+          type="number"
+          name="amount"
+          value={values.amount}
+          onChange={handleChange}
+        />
+        <Field
+          label="Notes"
+          placeholder="Additional notes"
+          type="text"
+          name="note"
+          value={values.note}
+          onChange={handleChange}
+        />
 
         <div className="flex gap-4">
           <Button>Save</Button>
